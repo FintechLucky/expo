6bcc357ea91805fe18908f17dd685cc7d33749b9
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-19 23:52:49 +0900

    design

--- src/inc/qrcode.js
+++ src/inc/qrcode.js
@@ -35,27 +35,30 @@ function QRCode(){
     }
 
         return (
-            <div className='container'>
+            <div className='qrcontainer'>
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
                     <span className='AppName'>F i n     P a y</span>
                     <span className='subTitle'>쉬 운  정 산,  빠 른  송 금</span>
-
-                    <span className='comment1'>송금 요청을 위한 카카오톡 송금 QR 코드</span>
-                    <span className='comment2'>이미지를 등록해주세요</span>
-                    <span className='comment3'>* 사진 불러오기 후 이미지 등록을 누르면 사진이 업로드 됩니다 *</span>
                 </div>
-                <label htmlFor='file' className='PhotoBtn'>
-                    <img className='Photoimg' src={require('../img/gallery.png').default} alt='talkimg'  />
-                    <span className='PhotoText'>사진 불러오기</span>
-                </label>
-                <input type='file' id='file' name='file' onChange={onChange}/>
-                <button className='QRBtn' onClick={onClick}>
-                    <img className='QRimg' src={require('../img/qr-code.png').default} alt='talkimg' />
-                    <span className='QRText'>송금 QR 이미지 등록</span>
-                </button>
-                <button className='MethodBtn'>
-                    <span className='MethodText'>송금 QR 이미지 다운로드 방법</span>
-                </button>
+                <div className='buttons'>
+                    <div className='comments'>
+                        <span className='comment1'>송금 요청을 위한 카카오톡 송금 QR 코드</span>
+                        <span className='comment2'>이미지를 등록해주세요</span>
+                        <span className='comment3'>* 사진 불러오기 후 이미지 등록을 누르면 사진이 업로드 됩니다 *</span>
+                    </div>
+                    <label htmlFor='file' className='PhotoBtn'>
+                        <img className='Photoimg' src={require('../img/gallery.png').default} alt='talkimg'  />
+                        <span className='PhotoText'>사진 불러오기</span>
+                    </label>
+                    <input style={{display:"none"}} type='file' id='file' name='file' onChange={onChange}/>
+                    <button className='QRBtn' onClick={onClick}>
+                        <img className='QRimg' src={require('../img/qr-code.png').default} alt='talkimg' />
+                        <span className='QRText'>송금 QR 이미지 등록</span>
+                    </button>
+                    <button className='MethodBtn'>
+                        <span className='MethodText'>송금 QR 이미지 다운로드 방법</span>
+                    </button>
+                </div>
             </div>
         );
 }
