fb461951f021435edee43cc590d79cf10b68d033
yerin1106 <k67065411@gmail.com>
2021-08-19 16:43:26 +0900

    user profile

--- src/inc/main.js
+++ src/inc/main.js
@@ -8,7 +8,16 @@ class main extends Component {
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
                     <span className='AppNamemain'>Fin Pay</span>
                 </div>
+                
                 <img className='line' src={require('../img/substract.png').default} alt='talkimg' />
+                
+                <div className='containerprofile'>
+                    <img className='user' src={require('../img/user.png').default} alt='talkimg' />
+                    <span className='username'>고객명</span>
+                </div>
+                
+                <img className='line' src={require('../img/substract.png').default} alt='talkimg' />
+
             </div>
         );
     }
