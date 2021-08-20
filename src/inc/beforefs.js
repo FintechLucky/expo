d437151a5ed04eba5b61a9a8a6b43b71c22cd4ab
yerin1106 <k67065411@gmail.com>
2021-08-20 14:57:55 +0900

    make profile

--- src/inc/beforefs.js
+++ src/inc/beforefs.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../css/BeforeFS.css'
 
+var name = []
+var photo = []
 
 class beforefs extends Component {
     componentDidMount(){
@@ -15,9 +17,12 @@ class beforefs extends Component {
             success: function(response) {
               
               for(var step = 0; step<response.elements.length; step++){
-                  console.log(response.elements[step].profile_nickname)
-                  console.log(response.elements[step].profile_thumbnail_image)
+                  name.push(response.elements[step].profile_nickname)
+                  photo.push(response.elements[step].profile_thumbnail_image)
               }
+              console.log(name)
+              console.log(photo)
+
             },
             fail: function(error) {
               console.log(error);
@@ -25,9 +30,36 @@ class beforefs extends Component {
           });
       }
 
+    display = () => {
+        return 
+    }
+
+    makeprofile =() => {
+        for(var step = 0; step<name.length; step++){
+            if (photo[step] = ""){
+                return (
+                    <div className='container_profile_before'>
+                        <img className='user_main' src={require('../img/user.png').default} alt='talkimg' />
+                        <span className='username_before'>name[step]</span>
+                    </div>
+                )
+            }
+            else{
+                return(
+                    <div className='container_profile_before'>
+                        <img className='user_main' src={require(photo[step]).default} alt='talkimg' />
+                        <span className='username_before'>name[step]</span>
+                    </div>
+                )
+
+            }
+        }
+    }
+
     handleChange =(e)=>{
         console.log(e.target.value);
     } //console 출력 안됨 -> 나중에 해결...
+
 
     render() {  
 
@@ -35,7 +67,7 @@ class beforefs extends Component {
             <div className='container_before'>
                 <span className='AppName_before'>친구 선택</span>
                 <input type={"text"} className={"input_name"} onchange={this.handleChange}/>
-
+                {this.makeProfile()}
             </div>
             
             
